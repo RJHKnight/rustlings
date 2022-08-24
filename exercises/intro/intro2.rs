00ee3cf0f2852bcf4531f3e9d1c59e61d5941916
Richard <rjhknight@gmail.com>
2022-08-24 16:42:42 +1000

    Another merge

--- exercises/intro/intro2.rs
+++ exercises/intro/intro2.rs
@@ -1,16 +1,8 @@
 // intro2.rs
 // Make the code print a greeting to the world.
-<<<<<<< Updated upstream
-// Execute `rustlings hint intro2` or use the `hint` watch subcommand for a hint.
-
-fn main() {
-    let name = "Richard";
-    println!("Hello {}!", name);
-=======
 // Execute `rustlings hint intro2` for a hint.
 
 
 fn main() {
     println!("Hello {}!", "Richard");
->>>>>>> Stashed changes
 }
