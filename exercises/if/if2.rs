fa3e700cba27cf0bef3b0a17faae5b32c4eb9a6f
Richard <rjhknight@gmail.com>
2022-08-25 18:28:09 +1000

    Not sure why this changed!

--- exercises/if/if2.rs
+++ exercises/if/if2.rs
@@ -21,16 +21,16 @@ mod tests {
 
     #[test]
     fn foo_for_fizz() {
-        assert_eq!(foo_if_fizz("fizz"), "foo")
+        assert_eq!(fizz_if_foo("fizz"), "foo")
     }
 
     #[test]
     fn bar_for_fuzz() {
-        assert_eq!(foo_if_fizz("fuzz"), "bar")
+        assert_eq!(fizz_if_foo("fuzz"), "bar")
     }
 
     #[test]
     fn default_to_baz() {
-        assert_eq!(foo_if_fizz("literally anything"), "baz")
+        assert_eq!(fizz_if_foo("literally anything"), "baz")
     }
 }
